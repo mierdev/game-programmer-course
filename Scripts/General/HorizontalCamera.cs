864a71128eff6f371cf7c4a8066ab4ef854f9863
Miranda <105456582+mierdev@users.noreply.github.com>
2022-10-04 22:21:03 +0200

    Add files via upload

---
+++ Scripts/General/HorizontalCamera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HorizontalCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    void Update()
+    {
+        transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);
+    }
+}
